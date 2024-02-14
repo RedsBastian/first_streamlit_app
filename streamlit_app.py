d0819ae6a1f4005fcee718e72402bc33d97871e7
RedsBastian <157044414+RedsBastian@users.noreply.github.com>
2024-02-14 12:02:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 streamlit.title("nuevo texto")
 streamlit.header("subtitulo")
 streamlit.text("aca va el texto")
-streamlit.header("subtitulo2 :)")
+streamlit.header("🥭 Frutas🍉 ")
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
